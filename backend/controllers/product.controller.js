1f6078a81470b588fabfc52ed5cba7caacd66bb4
jnp000 <jnp000@student.bham.ac.uk>
2024-11-20 19:18:13 +0000

    port now based on env variable, update to DELETE route

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -39,6 +39,7 @@ export const updateProduct = async (req, res) => {
     if (!mongoose.Types.ObjectId.isValid(id)) {
         return res.status(404).json({ success: false, message: "Product not found" });
     }
+
     try {
         const updated = await Product.findByIdAndUpdate(id, product, { new: true });
         res.status(200).json({ success: true, data: updated });
@@ -53,11 +54,15 @@ export const deleteProduct = async (req, res) => {
     const {id} = req.params
     console.log("ID to delete: ", id)
 
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: false, message: "Product not found" });
+    }
+    
     try {
         await Product.findByIdAndDelete(id);
         res.status(200).json({ success: true, message: "Product deleted" });
     } catch (error){
         console.log("ERROR: ", error.message);
-        res.status(404).json({ success: false, message: "Product not found" });
+        res.status(500).json({ success: false, message: "Server Error" });
     }
 }
--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,14 @@ import { connectDB } from './config/db.js';
 import productRoutes from './routes/product.route.js';
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); //middleware, allows us to accept JSON in req.body
 
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server running");
+    console.log("Server running http://localhost:" + PORT);
 })
 

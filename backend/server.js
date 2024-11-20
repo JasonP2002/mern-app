8d1441ee740d739411439122fec78df7dceb97de
jnp000 <jnp000@student.bham.ac.uk>
2024-11-20 19:12:57 +0000

    added GET ALL, UPDATE and DELETE routes

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,13 @@
 import express from 'express';
+
 import { connectDB } from './config/db.js';
-import Product from './models/product.model.js';
+import productRoutes from './routes/product.route.js';
 
 const app = express();
+
 app.use(express.json()); //middleware, allows us to accept JSON in req.body
 
-app.post("/api/products", async (req, res) => {
-    const product = req.body; //user sends this
-
-    if (!product.name || !product.price || !product.image) {
-        return res.status(400).json({ success: false, message: "Please provide all fields" });
-    }
-
-    const newProduct = new Product(product);
-    try {
-        await newProduct.save();
-        res.status(201).json({ success: true, data: newProduct});
-    } catch (error) {
-        console.error("ERROR:", error.message);
-        res.status(500).json({ success: false, message: "Server Error" });
-    }
-})
+app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
     connectDB();

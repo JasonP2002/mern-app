ff4b3da90ca94928250536b9e3401f5802568761
jnp000 <jnp000@student.bham.ac.uk>
2024-11-20 11:43:59 +0000

    mongoDB connection

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # mern-app
+
+### To Run
+Create .env file with a MongoDB connection string saved as MONGO_URI
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import { connectDB } from './config/db.js';
 
 const app = express();
+
+app.get("/products", (req, res) => {
+    
+})
+
 app.listen(5000, () => {
+    connectDB();
     console.log("Server running");
 })
+

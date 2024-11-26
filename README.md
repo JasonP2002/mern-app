3c9396ebe6612410c04edcf8cd3075241d7a311e
jnp000 <jnp000@student.bham.ac.uk>
2024-11-26 18:33:41 +0000

    added scripts, ready for deployment

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # mern-app
 
 ### To Run
-Create .env file with a MongoDB connection string saved as MONGO_URI
+Create .env file with a MongoDB connection string saved as `MONGO_URI`, and whichever port you would like the application to run on saved as `PORT`.
+
+Run command `npm run start` in your terminal and the application will run on `http://localhost:PORT`, where `PORT` should be the port you saved in the .env file.
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 
 import { connectDB } from './config/db.js';
 import productRoutes from './routes/product.route.js';
@@ -6,9 +7,19 @@ import productRoutes from './routes/product.route.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); //middleware, allows us to accept JSON in req.body
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     connectDB();

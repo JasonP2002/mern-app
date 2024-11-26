7d4e679ab4b2b63aaf0769b92fdb9398c8a2abe8
jnp000 <jnp000@student.bham.ac.uk>
2024-11-26 12:50:45 +0000

    home page link to create page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,30 @@
+import { Container, SimpleGrid, Text, VStack } from '@chakra-ui/react';
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
     return (
-        <div></div>
+        <Container maxW="container.x1" py={12}>
+            <VStack spacing={8}>
+                
+                <Text fontSize={"30"} fontWeight={"bold"}
+                        bgGradiant={"linear(to-r, cyan.400, blue.500)"}
+                        bgClip={"text"} texAlign={"center"}>
+                        
+                        Current Products 🚀
+                </Text>
+
+                <Text fontSize="xl" textAlign={"center"} fontWeight="bold" color="gray.500">
+                    No products found. {" "}
+
+                    <Link to={"/create"}>
+                        <Text as="span" color="blue.500" _hover={{ textDecoration: "underline" }}>
+                            Create a new product
+                        </Text>
+                    </Link>
+
+                </Text>
+            </VStack>
+        </Container>
     )
 }
 export default HomePage;
